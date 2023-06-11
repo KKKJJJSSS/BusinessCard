76a665a3bb380031f1841d9ee357bed374d5b788
KKKJJJSSS <kj971023@naver.com>
2023-06-11 20:06:40 +0900

    230611

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,19 @@
+const options = {
+    root: null, // viewport
+    rootMargin: "0px",
+    threshold: 1.0,  // 50%가 viewport에 들어와 있어야 callback 실행
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        } else {
+            entry.target.classList.remove('active');
+        }
+    });
+}, options);
+
+const boxList = document.querySelectorAll('.box');
+
+boxList.forEach(el => observer.observe(el));
